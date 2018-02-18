7ef881dee42036bba4f5ada1dc80a015f37e1c69
Rocie Carrillo <rocie@hotmail.com>
2018-02-17 17:27:29 -0800

    Items collecting data for DB

---
+++ ecarousel-grid/Model/Greetings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ecarousel-grid.Model
+{
+    public class Greetings //Name of the Model; collection of all the greetings users create
+{
+
+}
+}
